999eb8c5268167b7841674e1fb27c6b2ba8c3ba4
Olaoluwa Thompson <137652527+codakyello@users.noreply.github.com>
2025-05-26 04:24:10 +0100

    session

--- test.js
+++ test.js
@@ -1,7 +1,18 @@
 const dotenv = require("dotenv");
+const express = require("express");
+
+const app = express();
 
 dotenv.config({ path: `${__dirname}/config.env` });
 
 const startTelegramBot = require("./bot");
 
-startTelegramBot();
+// Server Port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+  startTelegramBot().catch((err) => {
+    console.error("Telegram bot failed to start", err);
+  });
+});
+// startTelegramBot();
